304d9bfee2f1ad843a0ae4497dfdd82f25f00b3a
Miraç Doğan <miracdogn@gmail.com>
2024-06-19 23:26:32 +0300

    Configurations and Seed Data

---
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nlayer.Core;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Seeds
+{
+    internal class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(new Product
+            {
+                Id = 1,
+                CategoryId = 1,
+                Name = "Pencil 1",
+                Price = 100,
+                Stock = 20,
+                CreatedDate = DateTime.Now
+            },
+            new Product
+            {
+                Id = 2,
+                CategoryId = 1,
+                Name = "Pencil 2",
+                Price = 200,
+                Stock = 30,
+                CreatedDate = DateTime.Now
+            },
+            new Product
+            {
+                Id = 3,
+                CategoryId = 1,
+                Name = "Pencil 3",
+                Price = 300,
+                Stock = 40,
+                CreatedDate = DateTime.Now
+            },
+            new Product
+            {
+                Id = 4,
+                CategoryId = 2,
+                Name = "Notebook 1",
+                Price = 500,
+                Stock = 60,
+                CreatedDate = DateTime.Now
+            },
+            new Product
+            {
+                Id = 5,
+                CategoryId = 2,
+                Name = "Notebook 2",
+                Price = 900,
+                Stock = 60,
+                CreatedDate = DateTime.Now
+            });
+        }
+    }
+}
